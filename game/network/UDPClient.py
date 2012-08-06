11d4e54f7651ef94dddc8951a2b531de38d5a3ad
sidcostas <email@email.com>
2012-08-06 15:07:57 -0700

    forgot to add the new files !

---
+++ game/network/UDPClient.py
@@ -0,0 +1,67 @@
+
+#!/usr/bin/env python
+
+"""AsyncClient.py: asynchronous networking client"""
+
+__author__      = "Konstantinos Sideris"
+__copyright__   = "Copyright 2012, UCLA game lab"
+
+
+
+import socket,pickle,time,string,sys
+try:
+    from  game.clientEnvironment import *
+except:
+    pass
+
+SHOW_STATISTICS =False
+ID = 0
+
+
+class UDPClient():
+    def __init__(self,client):
+        self.myclient = client
+        
+    def MakeRequest(self,pid,team,action,position):
+        #global ID
+        #ID  = pid
+        start =time.time()
+        message = str(self.server_address)+'&'+str(self.server_port+pid+1)+'&'+str(pid)+'&'+str(team)+'&'+str(action)
+        #print message,action
+        if position<>None: 
+                message+='&'+str(position[0])+','+str(position[1])
+        message+='&'+str(time.time())
+        #print message
+        #client = HTTPClient(self.client,self.server_address,self.server_port, message)
+        #asyncore.loop()
+    
+        
+        try:
+            data, addr = self.sockin.recvfrom( 1024 ) # buffer size is 1024 bytes
+            #print "received data:", data
+            self.myclient.deSerialize(data)
+        except:
+            print "Unexpected error:", sys.exc_info()[0]
+
+        self.sockout.sendto( message , (self.server_address,self.server_port)  )
+        if(SHOW_STATISTICS):
+            print 'Time Taken:',time.time()-start
+
+   
+    
+    def start(self,serv_addr,serv_port,pid):
+        
+        self.server_address = serv_addr
+        self.server_port =   serv_port
+        self.sockin = socket.socket( socket.AF_INET, # Internet
+                      socket.SOCK_DGRAM ) # UDP
+        self.sockin.bind( (serv_addr,serv_port+pid+1) )
+        self.sockin.setblocking(False)
+        self.sockout = socket.socket( socket.AF_INET, # Internet
+                      socket.SOCK_DGRAM ) # UDP
+
+if __name__ == '__main__':
+    s=UDPClient(None)
+    s.start('127.0.0.1',80,2)
+    while(True):
+        s.MakeRequest(2,1,1,(0,1))
---
+++ game/network/UDPServer.py
@@ -0,0 +1,86 @@
+
+#!/usr/bin/env python
+
+"""server.py: The core networking module. Transmits the serialized state to the clients-phones and reads their states.
+
+"""
+
+__author__      = "Konstantinos Sideris"
+__copyright__   = "Copyright 2012, UCLA game lab"
+
+
+
+
+import logging
+
+import pickle,time,sys #TODO change to cPickle for speed
+import shelve,os,socket
+
+SERVERDATA= 'ServerState.db'
+CLIENTDATA= 'ClientState.db'
+
+
+
+
+class UDPServer():
+
+        
+        
+    def start(self,UDP_IP,UDP_PORT):
+                
+                
+
+
+
+                self.sockin = socket.socket( socket.AF_INET, # Internet
+                                      socket.SOCK_DGRAM ) # UDP
+                self.sockin.bind( (UDP_IP,UDP_PORT) )
+                self.sockin.setblocking(False)
+                self.sockout = socket.socket( socket.AF_INET, # Internet
+                                      socket.SOCK_DGRAM ) # UDP
+
+                
+                if os.path.exists(CLIENTDATA):
+                    os.remove(CLIENTDATA)
+    
+                while True:
+                    try:
+                        data, addr = self.sockin.recvfrom( 1024 ) # buffer size is 1024 bytes
+                        #print "received message:", data," from ",addr
+                        print (data.split('&')[0],data.split('&')[1])
+                        
+        
+                        serv_db = shelve.open(SERVERDATA)
+
+                        try:
+
+                            ServerState = serv_db['data']['string']             
+                            
+                            self.sockout.sendto( ServerState,(data.split('&')[0],int(data.split('&')[1])) )
+                            #self.sockout.sendto( 'Hello',(data.split('$')[0],int(data.split('$')[1])) )
+                        except:
+                            pass
+                            
+                        finally:
+                            serv_db.close()
+                            
+                        
+                        
+                        ClientState = data.split('&')[2]+'$'+data.split('&')[3]+'$'+data.split('&')[4]+'$'+data.split('&')[5]+'$'+data.split('&')[6]
+                        
+                       
+                        client_db = shelve.open(CLIENTDATA)
+                        try:
+                            client_db[data.split('&')[2]] = {'time':time.time(),'string':ClientState}
+                        finally:
+                            client_db.close()
+
+
+                    except:
+                        pass
+
+
+
+if __name__ == '__main__':
+    s=Server()
+    s.start('127.0.0.1',80)
